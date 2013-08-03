d24452f6258b8153c12c9143218100c50dfce1b9
Philipp Haußleiter <philipp.haussleiter@yahoo.de>
2013-08-03 22:52:32 +0200

    Update default.rb

--- cookbooks/smartos/attributes/default.rb
+++ cookbooks/smartos/attributes/default.rb
@@ -8,7 +8,7 @@ default[:ntp][:servers] = ["0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org"]
 ## SSH Pub/Priv Keys for root
 #default[:ssh][:pubkey]                         = "http://path/to/id_dsa.pub"
 #default[:ssh][:privkey]                        = "http://path/to/id_dsa"
-default[:ssh][:authorized_keys]                = [""]
+#default[:ssh][:authorized_keys]                = [""]
 
 ## DNS (Default to OpenDNS)
 default[:dns][:search]          = "local"
