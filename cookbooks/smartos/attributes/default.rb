0fdef1d7240a811d7823c1e424b0a574731e2435
Philipp Haußleiter <philipp.haussleiter@yahoo.de>
2013-08-03 22:45:47 +0200

    Update default.rb

--- cookbooks/smartos/attributes/default.rb
+++ cookbooks/smartos/attributes/default.rb
@@ -8,13 +8,13 @@ default[:ntp][:servers] = ["0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org"]
 ## SSH Pub/Priv Keys for root
 #default[:ssh][:pubkey]                         = "http://path/to/id_dsa.pub"
 #default[:ssh][:privkey]                        = "http://path/to/id_dsa"
-default[:ssh][:authorized_keys]                = ["ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAuh+pT8hiP9Sl4K0OGGiiIdZlJbDzIYD0rYM3FfOtVXP2L3lbfjJV2e90u6C6rI6TBfuzJKxKWYz1TwO73bln4Xz4miY7gqu6R8mvWwLejYNGEE9KBc2EP9R5YhetTGWrjub4AMiSuB6dvw3WzdKAj6Ng2NGZludCy+NCjExUlX/WgGmymFMrTYLSV4WyKjOYc4QA6FBCX87DjiulQVfTIWuesrQIm6zG4RSwe76gxnXPrKkT06jt38pX/W4CFUXmGwqIVRZ5jJk30mE13EA2/V2yh0y+ksbY3bSyqUzU7wQQjXjQJQf9rIHbJvR5cgqxL53kpywmeUzbYScGYaVpQQ== m.goetzke@curasystems.de"]
+default[:ssh][:authorized_keys]                = [""]
 
 ## DNS (Default to OpenDNS)
-default[:dns][:search]          = "curasystems.local"
-default[:dns][:nameservers]     = ["208.67.220.220", "208.67.222.222"]
+default[:dns][:search]          = "local"
+default[:dns][:nameservers]     = ["8.8.8.8"]
 
 # default[:package][:carbonx][:name] = "py27-carbon-0.9.10"
 # default[:package][:carbonx][:version] = "0.9.10"
 
-default[:chef_source][:url]	= "http://192.168.3.114:8080"
+default[:chef_source][:url]	= "http://pkgsrc.smartos.org/packages"
